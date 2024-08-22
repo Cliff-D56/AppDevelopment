1787730ed11faba2c1d68866111521341fca8ab5
Cliff-D56 <cliffordgardner9@gmail.com>
2024-08-22 18:40:23 -0500

    setup

---
+++ JavaTest/lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,8 @@
+package com.example.lib;
+
+public class MyClass {
+    public static void main(String[] args) {
+        char hi = '5';
+        System.out.println("Char test: "+(hi+hi));
+    }
+}
