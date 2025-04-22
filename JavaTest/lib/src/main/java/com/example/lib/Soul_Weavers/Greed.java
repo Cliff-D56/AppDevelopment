d857d0a76aae42dec2ad8ff862b27873c7a0e0c9
Cliff-D56 <cliffordgardner9@gmail.com>
2025-04-21 23:53:14 -0500

    added Greed

---
+++ JavaTest/lib/src/main/java/com/example/lib/Soul_Weavers/Greed.java
@@ -0,0 +1,9 @@
+package com.example.lib.Soul_Weavers;
+
+public class Greed extends Soul_Weaver{
+    public Greed(String name,int age,Boolean deadOrAlive){
+        super(name,age,deadOrAlive);
+    }
+    public Greed(){}
+    public void sayHello(){};
+}
